965fc433e7b3f326a75d106277d39a5398ef7f47
JohnWilberforce <jacheampong45@yahoo.com>
2023-12-31 01:40:37 +0000

    finally

--- Models/AnwiamServicesContext.cs
+++ Models/AnwiamServicesContext.cs
@@ -55,7 +55,7 @@ public partial class AnwiamServicesContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-WILBERFO\\SQLEXPRESS;Database=AnwiamServices;Trusted_Connection=True;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-M1NK2O8\\SQLEXPRESS;Database=AnwiamServices;Trusted_Connection=True;TrustServerCertificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
