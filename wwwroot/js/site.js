4da5b6ef502de846857c46a807f896d1df443329
JohnWilberforce <jacheampong45@yahoo.com>
2024-01-03 14:22:24 +0000

    logic for revenue amount

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,25 +2,52 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-var check = document.querySelector('.check');
-check.addEventListener("click", checkTick);
+//var check = document.querySelector('.check');
+//check.addEventListener("click", checkTick);
 
-function checkTick(e){
+//function checkTick(e){
 
-    if (e.target.checked) {
+//    if (e.target.checked) {
 
-        check.checked = true;
+//        check.checked = true;
 
-        check.Disabled = true;
+//        check.Disabled = true;
 
+//    }
+//    else {
+
+//        e.preventDefault();
+//    }
+
+//}
+
+function calcSum() {
+    const multiselect = document.getElementById('services');
+    let selOptions = multiselect.selectedOptions;
+    console.log(selOptions[0].value);
+    let sum = 0;
+    for (let i = 0; i < selOptions.length; i++) {
+        if (selOptions[i].value == 'VFT') {
+            sum+= 150;
+        }
+        if (selOptions[i].value == 'OCT') {
+            sum+= 250;
+        }
+        if (selOptions[i].value == 'Macula') {
+            sum+= 100;
+        }
+        if (selOptions[i].value == 'Pachymetry') {
+            sum+= 100;
+        }
+        if (selOptions[i].value == 'RetinalImaging') {
+            sum+= 100;
+        }
+        if (selOptions[i].value == 'Frame') {
+            sum+= 0;
+        }
+        
+
+        let amount = document.getElementById('Amount');
+        amount.value = sum;
     }
-    else {
-
-        e.preventDefault();
-    }
-
 }
-//var navLink = document.querySelector('#op');
-//navLink.addEventListener('click', function (e) {
-//    this.classList.toggle('changeColor');
-//});
